4b58654333f49b2e7bb1a1adccc78776d724c9cc
Jeremy Ifrah <jeremy@ifrah.net>
2025-07-02 10:40:36 +0200

    fixing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -59,19 +59,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-// src/index.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/globals.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
