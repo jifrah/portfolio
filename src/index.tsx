7ae1a4eda7279d23dfe9be4f0f97cfb2d9e36fbe
Jeremy Ifrah <jeremy@ifrah.net>
2025-07-02 09:41:56 +0200

    src. index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/globals.css';  // Change this line from './index.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
